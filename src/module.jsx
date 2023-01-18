6c209556054808077b24c91b2ae218d0a613b7c9
Dmitriy <plotnikovdi95@gmail.com>
2023-01-18 13:50:11 +0300

    Сделали сборку

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import cat from './img/cat.jpeg'
+
+const Module = () => (
+  <>
+    <h1>Hello React</h1>
+    <img width={500} src={cat} alt="" />
+  </>
+)
+
+export default Module
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const mode = process.env.NODE_ENV || 'development'
+
+module.exports = {
+  mode,
+  devtool: mode === 'development' ? 'eval' : false,
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: '[name][contenthash].js',
+    assetModuleFilename: 'assets/[hash][ext][query]',
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name][contenthash].css',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/i,
+        exclude: /node_modules/i,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['postcss-preset-env'],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext]',
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ],
+  },
+}
